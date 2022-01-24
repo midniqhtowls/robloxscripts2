5cacf0d015ac59332118b47efeba4eb2b2817206
midniqhtowls <89306314+midniqhtowls@users.noreply.github.com>
2022-01-24 21:03:34 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,11 +1,4 @@
 whitelist = { 
-    ["sashasmoon"] = 418237714, -- cool person
-    ["untitledtxt"] = 2412014739, -- cool name
-    ["cqthi"] = 136051926, -- cool name
-    ["carvenyiou"] = 36770, -- cool name
-    ["zensityy"] = 530191889, -- cool name
-    ["miidwxst"] = 216174989, -- me
-    ["Prvbably"] = 587163717, -- cool name
-    ["Voyqi"] = 2255706751, -- cool name
+    [826506326] -- miidwxst
 }
 return whitelist
