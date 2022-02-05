c725da7588d158d6b5fed3b734c568581f066286
midniqhtowls <89306314+midniqhtowls@users.noreply.github.com>
2022-02-05 14:02:03 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,7 +1,7 @@
-whitelist = { 
+testers = { 
     [216174989] = true, -- miidwxst
     [3054381012] = true, -- Idxntcxreluv
     [2487262794] = true, -- InactiveCalls
     [2315404251] = true, -- Devilpathsfan
 }
-return whitelist
+return testers
