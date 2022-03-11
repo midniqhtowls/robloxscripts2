7d374579c78cb67719959929c2ae5400ad97c850
9sw <89306314+midniqhtowls@users.noreply.github.com>
2022-03-11 15:26:41 +0700

    Update innit.lua

--- innit.lua
+++ innit.lua
@@ -2,8 +2,6 @@ whitelist = {
     ["miidwxst"] = 216174989,
     ["Idxntcxreluv"] = 3054381012,
     ["grontrow"] = 1344373244,
-    ["InactiveCalls"] = 2487262794,
-    ["Devilpathsfan"] = 2315404251,
     ["ChristopherGarza73"] = 247880521, 
 }
 return whitelist
