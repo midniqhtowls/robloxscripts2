161f9190623ee179c6dce526ad4d2619725148e5
9sw <89306314+midniqhtowls@users.noreply.github.com>
2022-03-12 16:10:37 +0700

    Update innit.lua

--- innit.lua
+++ innit.lua
@@ -4,5 +4,7 @@ whitelist = {
     ["grontrow"] = 1344373244,
     ["ChristopherGarza73"] = 247880521, 
     ["DavidAtkinson88"] = 243100005,
+    ["dmyzy"] = 1366200493,
+    ["MrCupcake_victor"] = 1584233795,
 }
 return whitelist
